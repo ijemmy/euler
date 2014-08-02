4a185500bb9d7970c74d7c41151af62e1ac5c515
ijemmy <ijemmy@users.noreply.github.com>
2014-08-01 21:11:18 -0400

    First commit from quickstart archtype of maven

---
+++ euler/src/main/java/com/ijemmy/euler/App.java
@@ -0,0 +1,13 @@
+package com.ijemmy.euler;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
