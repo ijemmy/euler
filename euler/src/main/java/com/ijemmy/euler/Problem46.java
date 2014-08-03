8d90965e17f93635933c173552c5e6772d76d541
ijemmy <ijemmy@users.noreply.github.com>
2014-08-03 17:54:35 -0400

    Add problem 46

---
+++ euler/src/main/java/com/ijemmy/euler/Problem46.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package com.ijemmy.euler;
+
+/**
+ * We will iterate through all odd number until we find the number that break Goldbach's conjecture
+ * For each number, we will
+ * 1. Check that it is not a prime number (must be composite)
+ * 2. Iterate through a list of prime numbers with smaller value than the current number
+ * 2.1 Subtract the number with the prime number and divide by 2
+ * 2.2 Determine if this number is a square root of another integer
+ * 
+ * Note: There is a faster way to determine if an integer is a square of another integer
+ * The discussion can be found here
+ * http://stackoverflow.com/questions/295579/fastest-way-to-determine-if-an-integers-square-root-is-an-integer
+ * 
+ * @author ijemmy
+ *
+ */
+public class Problem46 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
