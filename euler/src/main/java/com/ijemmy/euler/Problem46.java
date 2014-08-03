d54867c1d0d054468e3111308c9bc8c47108b82f
ijemmy <ijemmy@users.noreply.github.com>
2014-08-03 18:07:42 -0400

    Problem 46 solved

--- euler/src/main/java/com/ijemmy/euler/Problem46.java
+++ euler/src/main/java/com/ijemmy/euler/Problem46.java
@@ -3,6 +3,8 @@
  */
 package com.ijemmy.euler;
 
+import com.ijemmy.euler.lib.Prime;
+
 /**
  * We will iterate through all odd number until we find the number that break Goldbach's conjecture
  * For each number, we will
@@ -10,6 +12,7 @@ package com.ijemmy.euler;
  * 2. Iterate through a list of prime numbers with smaller value than the current number
  * 2.1 Subtract the number with the prime number and divide by 2
  * 2.2 Determine if this number is a square root of another integer
+ * If there is a number that do not pass the test in 2.2 for all prime. That number is the smallest one that break Goldbach's conjecture
  * 
  * Note: There is a faster way to determine if an integer is a square of another integer
  * The discussion can be found here
@@ -20,12 +23,43 @@ package com.ijemmy.euler;
  */
 public class Problem46 {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int i = 3;
+		boolean foundNonGoldBach = false;
+		int smallestNonGoldBach = -1;
+		while(!foundNonGoldBach){
+			boolean foundGoldBach = false;
+			
+			if(Prime.isPrime(i)){
+				i += 2;
+				continue;
+			}
+			
+			int[] primeList = Prime.getListUntil(i); ///Can implement caching to improve performance
+			for(int prime : primeList){
+				int testNumber = (i - prime) / 2;
+				if(isSquare(testNumber)){
+					foundGoldBach = true;
+				}
+			}
+			
+			if(!foundGoldBach){
+				foundNonGoldBach = true;
+				smallestNonGoldBach = i;
+			}
+			
+			i += 2;
+		}
+		
+		System.out.println("The smallest odd composite number that betrays Christian Goldbach is " + smallestNonGoldBach);
+	}
+	
+	public static boolean isSquare(int n){
+		if (n < 0){
+		    return false;
+		}
+		long sqrt = (long)Math.sqrt(n);
+		return sqrt*sqrt == n;
 	}
 
 }
